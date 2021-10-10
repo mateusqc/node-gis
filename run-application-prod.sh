453e89de93461865bd793e7d25b13de77d02cadf
mateus.cunha <mateus.cunha@ccc.ufcg.edu.br>
2021-10-10 17:41:31 -0300

    ajuste script run-prod

--- run-application-prod.sh
+++ run-application-prod.sh
@@ -1,4 +1,5 @@
 container_id=$(docker run -d -p 15432:5432 --name nodegis-db mateusqc/nodegis-postgresql)
+echo ${container_id}
 db_ip=$(docker inspect $container_id | grep IPAddress | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' | tail -n1)
 machine_ip=$(hostname -I | tr " " "\n" | head -n1)
 docker run -d -p 8080:80 -e BASE_API_URL="http://${machine_ip}:8081" --name nodegis-fe mateusqc/nodegis-fe:latest
